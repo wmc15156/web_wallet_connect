8c7408ebb9575df801699981c378c5b854b73ee9
wmc15156 <wmc15156@naver.com>
2022-02-06 17:40:10 +0900

    페이지 진입시 메타마스크 여부 확인
    --- COMMIT END ---

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import './App.css';
 
 import PriorityExample from '../src/components/connectors/PriorityExample';
 import MetaMaskCard from '../src/components/connectors/MetaMaskCard';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
+
+    const [currentAccount, setCurrentAccount] = useState(null);
+
+    const checkWalletIsConnected = async () => {
+        const { ethereum } = window;
+        if (!ethereum) {
+            alert('Make sure you have Metamask installed');
+        } else {
+            console.log("Wallet exists! We're ready to go!");
+        }
+        try {
+            const accounts = await ethereum.request({ method: 'eth_accounts' });
+            if (accounts.length !== 0) {
+                const account = accounts[0];
+                console.log('Found an authorized: ', account);
+                setCurrentAccount(account);
+            } else {
+                console.log('No authorized account found');
+            }
+        } catch (err) {
+            window.location.href = 'https://metamask.io/';
+        }
+    };
+
+    useEffect(() => {
+        checkWalletIsConnected();
+    }, []);
+
+
+    return (
     <div>
       <PriorityExample />
       <div>
